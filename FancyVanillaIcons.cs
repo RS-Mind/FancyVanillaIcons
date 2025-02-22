00a38e5ce57a95242acc367fe7a8ac2f74d1f61c
RS_Mind <jared@awoodfamily.com>
2025-02-22 05:30:04 -0700

    Initial commit

---
+++ FancyVanillaIcons.cs
@@ -0,0 +1,54 @@
+﻿using BepInEx;
+using HarmonyLib;
+using UnboundLib.GameModes;
+using Jotunn.Utils;
+using UnityEngine;
+using System.Collections;
+using UnboundLib;
+using FancyCardBar;
+
+namespace FancyVanillaIcons
+{
+    [BepInDependency("com.willis.rounds.unbound", BepInDependency.DependencyFlags.HardDependency)]
+    [BepInDependency("com.rsmind.rounds.fancycardbar", BepInDependency.DependencyFlags.HardDependency)]
+    [BepInPlugin(ModId, ModName, Version)]
+    [BepInProcess("Rounds.exe")]
+    public class FancyVanillaIcons : BaseUnityPlugin
+    {
+        private const string ModId = "com.rsmind.rounds.fancyvanillaicons";
+        private const string ModName = "Fancy Vanilla Icons";
+        public const string Version = "1.0.0";
+        public const string ModInitials = "FVI";
+        public static FancyVanillaIcons instance { get; private set; }
+
+        void Awake()
+        {
+            Unbound.RegisterClientSideMod(ModId);
+            var harmony = new Harmony(ModId);
+            harmony.PatchAll();
+        }
+
+
+        void Start()
+        {
+            instance = this;
+
+
+            assets = AssetUtils.LoadAssetBundleFromResources("fancyvanillaicons", typeof(FancyVanillaIcons).Assembly);
+
+            if (assets == null)
+            {
+                UnityEngine.Debug.Log("Failed to load Fancy Vanilla Icons asset bundle");
+            }
+
+            foreach (CardInfo cardInfo in CardChoice.instance.cards)
+            {
+                FancyIcon icon = cardInfo.gameObject.AddComponent<FancyIcon>();
+                icon.fancyIcon = assets.LoadAsset<GameObject>("I_" + cardInfo.cardName.ToLower());
+            }
+        }
+
+        public static bool Debug = false;
+        internal static AssetBundle assets;
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Fancy Vanilla Icons
+Uses Fancy Card Bar to give fancy icons to all vanilla cards.
